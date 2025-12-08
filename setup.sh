10bd9043068c39cc4b14d0967063c2f89d655fc9
fritz <fritz.schinkel@t-online.de>
2025-12-08 23:12:08 +0100

    sudo usage changed

--- setup.sh
+++ setup.sh
@@ -21,13 +21,13 @@ sudo apt update && sudo apt -y upgrade
 sudo apt -y install dhcpcd dnsmasq hostapd git python3-venv python3-pip python3-full
 
 echo "[2/10] Creating JupyterLab environment..."
-mkdir -p /opt/jupyterlab
-python3 -m venv /opt/jupyterlab/venv
+sudo mkdir -p /opt/jupyterlab
+sudo chown "${JUP_USER}" /opt/jupyterlabpython3 -m venv /opt/jupyterlab/venv
 /opt/jupyterlab/venv/bin/pip install --upgrade pip
 /opt/jupyterlab/venv/bin/pip install jupyterlab
 
 echo "[3/10] Creating JupyterLab systemd service..."
-sudo cat <<EOF >/etc/systemd/system/juplabd.service
+sudo bash -c "cat > /etc/systemd/system/juplabd.service <<EOF 
 [Unit]
 Description=JupyterLab Server
 After=network.target
@@ -41,25 +41,25 @@ Restart=always
 
 [Install]
 WantedBy=multi-user.target
-EOF
+EOF"
 
 echo "[4/10] Creating dhcpcd configuration..."
-sudo cat <<EOF >/etc/dhcpcd.conf
+sudo bash -c "cat > /etc/dhcpcd.conf <<EOF
 interface wlan0
 static ip_address=${AP_IP}/24
 nohook wpa_supplicant
-EOF
+EOF"
 
 echo "[5/10] Creating dnsmasq configuration..."
-sudo cat <<EOF >/etc/dnsmasq.conf
+sudo bash -c "cat > /etc/dnsmasq.conf <<EOF
 interface=wlan0
 dhcp-range=${RANGE_START},${RANGE_END},255.255.255.0,24h
 dhcp-option=3   # no router
 dhcp-option=6   # no DNS (local only)
-EOF
+EOF"
 
 echo "[6/10] Creating hostapd configuration..."
-sudo cat <<EOF >/etc/hostapd/hostapd.conf
+sudo bash -c "cat > /etc/hostapd/hostapd.conf <<EOF
 interface=wlan0
 ssid=${AP_SSID}
 hw_mode=g
@@ -71,112 +71,112 @@ wpa_passphrase=${AP_WIFI_PASS}
 wpa_key_mgmt=WPA-PSK
 rsn_pairwise=CCMP
 driver=nl80211
-EOF
+EOF"
 
 sudo sed -i 's|#DAEMON_CONF=.*|DAEMON_CONF="/etc/hostapd/hostapd.conf"|' /etc/default/hostapd
 
 echo "7/10] Creating AP ON/OFF scripts in /usr/local/bin ..."
 
-sudo cat <<'EOF' >/usr/local/bin/ap_on
+sudo bash -c "cat > /usr/local/bin/ap_on <<EOF
 #!/bin/bash
 
-echo "Stopping NetworkManager ..."
+echo 'Stopping NetworkManager ...'
 sudo systemctl stop NetworkManager.service 2>/dev/null
 
-echo "Stopping wpa_supplicant ..."
+echo 'Stopping wpa_supplicant ...'
 sudo systemctl stop wpa_supplicant.service
 
-echo "Restarting dhcpcd ..."
+echo 'Restarting dhcpcd ...'
 sudo systemctl restart dhcpcd
 
-echo "Restarting dnsmasq ..."
+echo 'Restarting dnsmasq ...'
 sudo systemctl restart dnsmasq
 sleep 2
 
-echo "Restarting hostapd ..."
+echo 'Restarting hostapd ...'
 sudo systemctl restart hostapd
 
-echo "📡 Access Point aktiv → SSID: ${AP_SSID} ({AP_IP})"
+echo '📡 Access Point aktiv → SSID: ${AP_SSID} ({AP_IP})'
 sudo systemctl status --no-page hostapd
-EOF
+EOF"
 sudo chmod +x /usr/local/bin/ap_on
 
-sudo cat <<'EOF' >/usr/local/bin/ap_off
+sudo bash -c "cat > /usr/local/bin/ap_off <<EOF
 #!/bin/bash
 
-echo "Stopping hostapd ..."
+echo 'Stopping hostapd ...'
 sudo systemctl stop hostapd
 
-echo "Stopping dnsmasq ..."
+echo 'Stopping dnsmasq ...'
 sudo systemctl stop dnsmasq
 
-echo "Restarting dhcpcd ..."
+echo 'Restarting dhcpcd ...'
 sudo systemctl restart dhcpcd
 
-echo "Starting wpa_supplicant ..."
+echo 'Starting wpa_supplicant ...'
 sudo systemctl start wpa_supplicant.service
 
-echo "Starting NetworkManager ..."
+echo 'Starting NetworkManager ...'
 sudo systemctl start NetworkManager.service 2>/dev/null
-echo "📶 WLAN client active → Internet accessible"
-EOF
+echo '📶 WLAN client active → Internet accessible'
+EOF"
 sudo chmod +x /usr/local/bin/ap_off
 
 echo "[8/10] Creating show_ap_config..."
 
-sudo cat <<'EOF' >/usr/local/bin/show_ap_config
+sudo bash -c "cat > /usr/local/bin/show_ap_config <<EOF
 #!/bin/bash
-echo "====== ACCESS POINT CONFIGURATION ================================"
+echo '====== ACCESS POINT CONFIGURATION ================================'
 
-echo "====== dhcpcd ===================================================="
-echo "/etc/dhcpcd.conf"
+echo '====== dhcpcd ===================================================='
+echo '/etc/dhcpcd.conf'
 sudo cat /etc/dhcpcd.conf
 systemctl status dhcpcd --no-pager
 
 echo
-echo "====== dnsmasq ==================================================="
-echo "/etc/dnsmasq.conf"
+echo '====== dnsmasq ==================================================='
+echo '/etc/dnsmasq.conf'
 sudo cat /etc/dnsmasq.conf
 systemctl status dnsmasq --no-pager
 
 echo
-echo "====== hostapd ==================================================="
-echo "/etc/hostapd/hostapd.conf"
+echo '====== hostapd ==================================================='
+echo '/etc/hostapd/hostapd.conf'
 sudo cat /etc/hostapd/hostapd.conf
 systemctl status hostapd --no-pager
 
 echo
-echo "====== NetworkManager ============================================"
-#echo "/etc/?.conf"
+echo '====== NetworkManager ============================================'
+#echo '/etc/?.conf'
 #sudo cat /etc/?.conf
 systemctl status NetworkManager --no-pager
 
 echo
-echo "====== wpa_supplicant ============================================"
-# echo "/etc/?.conf"
+echo '====== wpa_supplicant ============================================'
+# echo '/etc/?.conf'
 # sudo cat /etc/?.conf
 systemctl status wpa_supplicant --no-pager
 
 echo
-echo "====== juplabd ==================================================="
+echo '====== juplabd ==================================================='
 systemctl status juplabd --no-pager
-EOF
+EOF"
 sudo chmod +x /usr/local/bin/show_ap_config
 
 
 echo "[9/10] Enabling / disabling services..."
-systemctl unmask dhcpcd
-systemctl enable dhcpcd
-systemctl unmask dnsmasq
-systemctl enable dnsmasq
-systemctl unmask hostapd
-systemctl enable hostapd
-systemctl unmask juplabd.service
-systemctl enable juplabd.service
-systemctl unmask NetworkManager
-systemctl disable NetworkManager
-systemctl unmask wpa_supplicant
-systemctl disable wpa_supplicant
+sudo systemctl unmask dhcpcd
+sudo systemctl enable dhcpcd
+sudo systemctl unmask dnsmasq
+sudo systemctl enable dnsmasq
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl unmask juplabd.service
+sudo systemctl enable juplabd.service
+sudo systemctl unmask NetworkManager
+sudo systemctl disable NetworkManager
+sudo systemctl unmask wpa_supplicant
+sudo systemctl disable wpa_supplicant
 
 
 echo "[10/10] Setup complete. Do possible additional installations. after that reboot for start in AP mode."
